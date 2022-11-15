9fe8563b443639d0c08ff800f8ac3361a47af055
DevonLud <devonjludwig@gmail.com>
2022-11-15 12:23:22 -0700

    profile camera

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,26 +1,37 @@
 
-import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button} from 'react-native';
+import React, { useEffect,useState,useRef } from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button,TouchableOpacity} from 'react-native';
 import { Card, CardTitle, CardContent} from 'react-native-material-cards';
 import BarChart from 'react-native-bar-chart';
-// import Share from 'react-native-share';
+// TouchableOpacity
 
+import { Camera } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';//
 
-
-// const data = [
-//   [70, 0],
-//   [80,0],
-//   [110, 0],
-//   [100, 0],
-//   [280, 0],
-//   [80,0 ],
-//   [110, 0]
-
-// ];
-// labels
-// const horizontalData = ['S', 'M', 'T', 'W', 'T', 'F','S'];
+const cameraOptions={//
+  quality:0,
+  exif:false
+}//
 
 const Profile = (props) => {
+  const [userName,setUserName] = useState("");
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(null);
+  const cameraRef = useRef(null);
+  const [cameraReady,setCameraReady] = useState(false);//
+  useEffect(()=>{
+    const getUserName = async ()=>{
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      setCameraPermission(cameraPermission);
+      const username = AsyncStorage.getItem('userName');
+      setUserName(userName);
+      await AsyncStorage.removeItem('profilePhoto')//temp
+      const profilePhoto = await AsyncStorage.getItem('profilePhoto');//
+      setProfilePhoto(profilePhoto);//
+    }
+    getUserName();
+  },[]);
+
   const myCustomerShare = async() =>{
     const shareOptions = {
       message: 'This is a test'
@@ -33,7 +44,27 @@ const Profile = (props) => {
   console.log('Error', error)
       }
     }
+    if(profilePhoto==null){//
+      return (
+        <View style={styles.container}>
+          <Camera style={styles.camera} ref={cameraRef} onCameraReady={()=>{setCameraReady(true)}}>
+            <View style={styles.buttonContainer}>
+              {cameraReady?<TouchableOpacity style={styles.button} onPress={async ()=> {
 
+                const picture = await cameraRef.current.takePictureAsync(cameraOptions);
+                console.log('Picture',picture);
+                await AsyncStorage.setItem('profilePhoto',picture.uri);
+                setProfilePhoto(picture.uri);
+                }}>
+                <Text style={styles.text}>Take Picture</Text>
+              </TouchableOpacity>: null}
+            </View>
+          </Camera>
+        </View>
+      )
+      
+    }//
+else{
   return (
     <SafeAreaView style={{flex: 1}}>
          <Card style={{backgroundColor:'white', borderRadius: 10, margin:20 ,width: 320, shadowColor: "#000",
@@ -44,11 +75,12 @@ shadowOffset: {
 shadowOpacity: 0.23,
 shadowRadius: 2.62,
 
+//line 82
 elevation: 4}}>
      <CardContent>
      <Image style={{height: 100, width:100, borderRadius: 75}}
-      source={require('../image/me.jpg')} />
-    <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>Sarah Romero</Text>
+      source={{uri:profilePhoto}} /> 
+    <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>{userName}</Text>
 
     <Text style={{marginTop:20,marginBottom:2}}>This Week's progress</Text>
 {/* <BarChart barColor='green' data={data} horizontalData={horizontalData} /> */}
@@ -59,11 +91,31 @@ elevation: 4}}>
     </Card>
  </SafeAreaView>
   );
-};
+}};
 export default Profile;
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20
-  }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
